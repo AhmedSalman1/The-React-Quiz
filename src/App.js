658d06a7f4204c3d18a8941c035d6f3c7d80a729
AhmedSalman <medosalman375@gmail.com>
2025-01-22 14:57:01 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const initialState = {
+	questions: [],
+
+	// 'loading', 'error', 'ready', 'active', 'finished'
+	status: 'loading',
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return {
+				...state,
+				questions: action.payload,
+				status: 'ready',
+			};
+		case 'dataFailed':
+			return {
+				...state,
+				status: 'error',
+			};
+		default:
+			throw new Error('Unknown action');
+	}
+}
+
+export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const res = await fetch('http://localhost:8000/questions');
+				const questions = await res.json();
+
+				dispatch({ type: 'dataReceived', payload: questions });
+				// console.log(questions);
+			} catch (error) {
+				// console.error('Error fetching questions:', error);
+				dispatch({ type: 'dataFailed' });
+			}
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div className="app">
+			<Header />
+
+			<Main>
+				<p>1/15</p>
+				<p>Question?</p>
+			</Main>
+		</div>
+	);
+}
