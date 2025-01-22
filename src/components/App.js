457b8bd32cd7576617e9e9461cd7ca0371decc58
AhmedSalman <medosalman375@gmail.com>
2025-01-22 20:24:09 +0200

    feat: add handling new answers and moving to next question

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,14 @@ import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
+import NextButton from './NextButton';
 
 const initialState = {
 	questions: [],
-
-	// 'loading', 'error', 'ready', 'active', 'finished'
-	status: 'loading',
+	status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
 	index: 0,
+	answer: null,
+	points: 0,
 };
 
 function reducer(state, action) {
@@ -32,13 +33,28 @@ function reducer(state, action) {
 				...state,
 				status: 'active',
 			};
+		case 'newAnswer':
+			const question = state.questions.at(state.index);
+
+			return {
+				...state,
+				answer: action.payload,
+				points:
+					action.payload === question.correctOption ? state.points + question.points : state.points,
+			};
+		case 'nextQuestion':
+			return {
+				...state,
+				index: state.index + 1,
+				answer: null,
+			};
 		default:
 			throw new Error('Unknown action');
 	}
 }
 
 export default function App() {
-	const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
 
 	const numQuestions = questions.length;
 
@@ -66,7 +82,12 @@ export default function App() {
 				{status === 'loading' && <Loader />}
 				{status === 'error' && <Error />}
 				{status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-				{status === 'active' && <Question question={questions[index]} />}
+				{status === 'active' && (
+					<>
+						<Question question={questions[index]} dispatch={dispatch} answer={answer} />
+						<NextButton dispatch={dispatch} answer={answer} />
+					</>
+				)}
 			</Main>
 		</div>
 	);
