543b22223c586f96f067438c86c2aefc2de2a447
AhmedSalman <medosalman375@gmail.com>
2025-01-22 16:10:31 +0200

    feat: handle loading, error, ready status and starting a new Quiz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
 	questions: [],
@@ -22,13 +26,20 @@ function reducer(state, action) {
 				...state,
 				status: 'error',
 			};
+		case 'start':
+			return {
+				...state,
+				status: 'active',
+			};
 		default:
 			throw new Error('Unknown action');
 	}
 }
 
 export default function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+	const numQuestions = questions.length;
 
 	useEffect(() => {
 		async function fetchData() {
@@ -51,8 +62,10 @@ export default function App() {
 			<Header />
 
 			<Main>
-				<p>1/15</p>
-				<p>Question?</p>
+				{status === 'loading' && <Loader />}
+				{status === 'error' && <Error />}
+				{status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+				{status === 'active' && <Question />}
 			</Main>
 		</div>
 	);
