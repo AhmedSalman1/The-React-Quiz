4136bf7dfbaaedf2eaa55e7e9cfb1b21efd3ddbe
AhmedSalman <medosalman375@gmail.com>
2025-01-23 07:57:03 +0200

    feat: add displaying progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
 	questions: [],
@@ -54,9 +55,13 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+		reducer,
+		initialState
+	);
 
 	const numQuestions = questions.length;
+	const maxPoints = questions.reduce((prev, curr) => prev + curr.points, 0);
 
 	useEffect(() => {
 		async function fetchData() {
@@ -84,6 +89,13 @@ export default function App() {
 				{status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
 				{status === 'active' && (
 					<>
+						<Progress
+							numQuestions={numQuestions}
+							index={index}
+							points={points}
+							maxPoints={maxPoints}
+							answer={answer}
+						/>
 						<Question question={questions[index]} dispatch={dispatch} answer={answer} />
 						<NextButton dispatch={dispatch} answer={answer} />
 					</>
